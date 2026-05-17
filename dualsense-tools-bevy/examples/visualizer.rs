f905c597ffdc74b67dab6d07048af89c7fae2c0a
Aldo Stracquadanio <aldo.stracquadanio@siriusxm.com>
2026-05-17 19:48:57 +0200

    Improve example and remove unnecessary dependencies for bevy plugin

--- dualsense-tools-bevy/examples/visualizer.rs
+++ dualsense-tools-bevy/examples/visualizer.rs
@@ -1,7 +1,6 @@
+use bevy::camera::ScalingMode;
 use bevy::camera::primitives::Aabb;
 use bevy::prelude::*;
-use bevy_third_person_camera::ThirdPersonCameraTarget;
-use bevy_third_person_camera::*;
 use dualsense_tools_bevy::{DualsenseTilt, DualsenseTiltPlugin};
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
@@ -20,101 +19,155 @@ struct GamepadBound {
 #[derive(Component)]
 struct ShowAxes;
 
-fn main() -> color_eyre::Result<()> {
-    color_eyre::install()?;
+fn main() {
     scene::<10>();
-    Ok(())
 }
 
 fn scene<const BUFSIZE: usize>() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugins(ThirdPersonCameraPlugin)
+        .add_plugins(DefaultPlugins.set(AssetPlugin {
+            file_path: "examples/assets".to_owned(),
+            ..default()
+        }))
         .add_plugins(DualsenseTiltPlugin::<BUFSIZE>)
         .add_systems(Startup, setup)
         .add_systems(Update, update::<BUFSIZE>)
         .add_systems(Update, draw_axes)
+        .add_systems(Update, attach_labels)
         .run();
+}
+
+fn spawn_tilt_visualizer(
+    commands: &mut Commands,
+    meshes: &mut ResMut<Assets<Mesh>>,
+    materials: &mut ResMut<Assets<StandardMaterial>>,
+    color: Color,
+    tilt_alg: TiltAlg,
+    transform: Transform,
+    text: impl Into<String>,
+    font: TextFont,
+) {
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::default())),
+        MeshMaterial3d(materials.add(StandardMaterial {
+            base_color: color,
+            metallic: 0.8,
+            ..default()
+        })),
+        transform,
+        GamepadBound { tilt_alg },
+        ShowAxes,
+        Node {
+            top: Val::Px(0.),
+            left: Val::Px(0.),
+            width: Val::Auto,
+            height: Val::Auto,
+            justify_content: JustifyContent::Center,
+            ..default()
+        },
+        Text::new(text),
+        TextColor(color),
+        TextBackgroundColor(Color::srgb(0.1, 0.1, 0.1)),
+        TextLayout::new_with_justify(Justify::Center),
+        font,
+    ));
+}
+
+fn attach_labels(
+    cubes: Query<(&Transform, &mut Node)>,
+    camera: Single<(Entity, &Camera)>,
+    transform_helper: TransformHelper,
+) {
+    let Ok(camera_transform) = transform_helper.compute_global_transform(camera.0) else {
+        warn!("Failed computing global transform for camera Entity");
+        return;
+    };
+
+    for (transform, mut node) in cubes {
+        let translation = transform.translation;
+        let global_transform = camera
+            .1
+            .world_to_viewport(
+                &camera_transform,
+                translation
+                    .with_y(translation.y + 1.0)
+                    .with_x(translation.x - 0.4),
+            )
+            .unwrap();
+        node.top = Val::Px(global_transform.y);
+        node.left = Val::Px(global_transform.x);
+    }
 }
 
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    asset_server: Res<AssetServer>,
 ) {
-    let camera_settings = ThirdPersonCamera {
-        mouse_orbit_button_enabled: true,
-        mouse_orbit_button: MouseButton::Right,
-        ..Default::default()
+    let font = asset_server.load("FantasqueSansMono-Bold.otf");
+    let text_font = TextFont {
+        font: font.clone(),
+        font_size: 13.0,
+        ..default()
     };
 
     commands.spawn((
         Camera3d::default(),
-        camera_settings,
-        Transform::from_xyz(0., 0., 6.),
+        Projection::from(OrthographicProjection {
+            scaling_mode: ScalingMode::FixedVertical {
+                viewport_height: 4.5,
+            },
+            ..OrthographicProjection::default_3d()
+        }),
+        Transform::from_xyz(0., 0., 3.).looking_at(Vec3::ZERO, Vec3::Y),
     ));
 
-    commands.spawn((
-        Mesh3d(meshes.add(Cuboid::default())),
-        MeshMaterial3d(materials.add(StandardMaterial {
-            base_color: Color::srgb(1., 0., 0.),
-            metallic: 0.8,
-            ..default()
-        })),
-        ThirdPersonCameraTarget,
-        Transform::from_xyz(-2., 1., 0.0),
-        GamepadBound {
-            tilt_alg: TiltAlg::AccelInstant,
-        },
-        ShowAxes,
-    ));
+    spawn_tilt_visualizer(
+        &mut commands,
+        &mut meshes,
+        &mut materials,
+        Color::srgb(1., 0., 0.),
+        TiltAlg::AccelInstant,
+        Transform::from_xyz(-2., 0.8, 0.),
+        "Accelerometer\nInstant Reading",
+        text_font.clone(),
+    );
 
-    commands.spawn((
-        Mesh3d(meshes.add(Cuboid::default())),
-        MeshMaterial3d(materials.add(StandardMaterial {
-            base_color: Color::srgb(0., 0., 1.),
-            metallic: 0.8,
-            ..default()
-        })),
-        ThirdPersonCameraTarget,
-        Transform::from_xyz(0., 1., 0.),
-        GamepadBound {
-            tilt_alg: TiltAlg::GyroInstant,
-        },
-        ShowAxes,
-    ));
+    spawn_tilt_visualizer(
+        &mut commands,
+        &mut meshes,
+        &mut materials,
+        Color::srgb(0., 0., 1.),
+        TiltAlg::GyroInstant,
+        Transform::from_xyz(0., 0.8, 0.),
+        "Gyroscope\nInstant Reading",
+        text_font.clone(),
+    );
 
-    commands.spawn((
-        Mesh3d(meshes.add(Cuboid::default())),
-        MeshMaterial3d(materials.add(StandardMaterial {
-            base_color: Color::srgb(0., 1., 0.),
-            metallic: 0.8,
-            ..default()
-        })),
-        ThirdPersonCameraTarget,
-        Transform::from_xyz(2., 1., 0.),
-        GamepadBound {
-            tilt_alg: TiltAlg::AccelAverage,
-        },
-        ShowAxes,
-    ));
+    spawn_tilt_visualizer(
+        &mut commands,
+        &mut meshes,
+        &mut materials,
+        Color::srgb(0., 1., 0.),
+        TiltAlg::AccelAverage,
+        Transform::from_xyz(2., 0.8, 0.),
+        "Accelerometer\nAverage",
+        text_font.clone(),
+    );
 
-    commands.spawn((
-        Mesh3d(meshes.add(Cuboid::default())),
-        MeshMaterial3d(materials.add(StandardMaterial {
-            base_color: Color::srgb(0., 1., 1.),
-            metallic: 0.8,
-            ..default()
-        })),
-        ThirdPersonCameraTarget,
-        Transform::from_xyz(0., -1., 0.),
-        GamepadBound {
-            tilt_alg: TiltAlg::AccelCorrectedGyro,
-        },
-        ShowAxes,
-    ));
+    spawn_tilt_visualizer(
+        &mut commands,
+        &mut meshes,
+        &mut materials,
+        Color::srgb(0., 1., 1.),
+        TiltAlg::AccelCorrectedGyro,
+        Transform::from_xyz(0., -1.2, 0.),
+        "Accelerometer-corrected\ngyroscope",
+        text_font.clone(),
+    );
 
-    commands.spawn((PointLight::default(), Transform::from_xyz(3.0, 8.0, 5.0)));
+    commands.spawn((PointLight::default(), Transform::from_xyz(0.0, 8.0, 7.0)));
 }
 
 fn draw_axes(mut gizmos: Gizmos, query: Query<(&Transform, &Aabb), With<ShowAxes>>) {
